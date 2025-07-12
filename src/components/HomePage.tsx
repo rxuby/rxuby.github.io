de864c7687c5da4d1d170f19d4a945a1fb08432b
rxuby <jutapon.c@kkumail.com>
2025-07-12 16:13:12 +0700

    [Edit] breakpoint

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import star from "../images/star.svg";
 import small2 from "../images/small2.png";
 import small4 from "../images/small4.png";
-import '../App.css'
+import "../App.css";
 
 function HomePage() {
   return (
@@ -40,7 +40,7 @@ function HomePage() {
           style={{ fontFamily: "Lilita One" }}
           className="box-shadow flex justify-center items-center text-[50px] p-1 px-9 mt-10 mb-1 md:text-7xl md:p-3 md:px-14 border-[3px] border-[#4c63af] bg-[#92a1d4] text-[#fbefe1]"
         >
-          Hi! I'm Khing 
+          Hi! I'm Khing
         </h1>
       </motion.div>
       <motion.div
@@ -51,13 +51,13 @@ function HomePage() {
       >
         <h1
           style={{ fontFamily: "Lilita One" }}
-          className="box-shadow flex justify-center items-center text-[45px] px-8 p-1 mb-1 md:p-3 md:px-14 md:text-7xl border-[3px] border-[#4c63af] bg-[#FF9494] text-[#fbefe1]"
+          className="box-shadow flex justify-center items-center text-center text-[45px] sm:text-[45px] px-8 p-1 mb-1 md:p-3 md:px-14 md:text-7xl border-[3px] border-[#4c63af] bg-[#FF9494] text-[#fbefe1]"
         >
           First Jobber & Front-End Dev
         </h1>
         <h1
           style={{ fontFamily: "Lilita One" }}
-          className="box-shadow flex justify-center items-center text-[39.5px] px-4 py-2 p-1  md:text-7xl md:p-3 md:px-14 mt-[3px] border-[3px] border-[#4c63af] bg-[#FFD1D1] text-[#fbefe1]"
+          className="box-shadow flex justify-center items-center text-center text-[45px] sm:text-[39.5px] px-4 py-2 p-1  md:text-7xl md:p-3 md:px-14 mt-[3px] border-[3px] border-[#4c63af] bg-[#FFD1D1] text-[#fbefe1]"
         >
           (Still polishing both layout and life)
         </h1>
@@ -72,7 +72,7 @@ function HomePage() {
           style={{ fontFamily: "Lilita One" }}
           className="text-shadow flex justify-end items-center text-[34px] p-2 px-2 mt-0 md:text-5xl md:p-3 md:px-14 md:mt-4 border-[#4c63af] text-[#FFBF66]"
         >
-          Designed & Developed by Khing (CS KKU)
+          Designed by Khing (CS KKU)
         </h1>
       </motion.div>
     </motion.div>
